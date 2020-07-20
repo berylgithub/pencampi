6c9e418b5596592a7d53c00c3f90524a6e49e3a0
berylramadhian <berylramadhian@gmail.com>
2020-07-20 18:44:59 +0700

    added parallel collective communication example

--- mpitest.py
+++ mpitest.py
@@ -7,6 +7,7 @@ Created on Wed Jul 15 13:49:14 2020
 
 from mpi4py import MPI
 import numpy as np
+import time
     
 def process_splitter(processes, cores):
     ###### algorithm to split processes to cores,just som algebra operations
@@ -44,61 +45,76 @@ def data_splitter(data, cores):
 def f_mul(a,b):
     return a*b
 
-if __name__ == "__main__":
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    
-    A = np.arange(23)
-    B = np.arange(24)[1:24]
-    sigma = 0
-    
-    X = None; Y = None
-    if rank == 0:
-        #split the data
-        X = data_splitter(A, size-1) #exclude 0th processor
-        Y = data_splitter(B, size-1)
-        print(X,Y)
-        print("dot result = ",np.dot(A,B))
-    #broadcast the splitted_data
-    X = comm.bcast(X, root=0) 
-    Y = comm.bcast(Y, root=0)
-    
-    ###arbitrary operations start here
-    if rank != 0:
-        y=0
-        for j in range(len(X[rank-1])):
-            y += f_mul(X[rank-1][j], Y[rank-1][j])
-            print("j",j, X[rank-1][j], Y[rank-1][j], y, rank)
-        comm.send(y, dest=0)
-            
-    if rank == 0:
-        for i in range(1, size):
-            y = comm.recv(source=i)
-            print("recv = ",y)
-            sigma += y
-        print("parallel result = ",sigma)
-    
-    
-#    if rank != 0:
-#        y = f_mul(X[rank-1], Y[rank-1])
-#        print(rank, y)
-#        comm.send(y, dest=0)
-#    elif rank == 0:
-#        for i in range(1,size):
-#            y_ = comm.recv(source=i)
-#            sigma+=y_
-#        print(rank, "result = ",sigma)
-#    sigma=comm.bcast(sigma, root=0)
-#    print("after broadcast =",sigma, "rank = ",rank)
-        
-    
+def dot(A,B):
+    ###pure single core func
+    return sum(A[i]*B[i] for i in range(len(A)))
 
-#    if rank == 0:
-#        data = np.arange(11)
-#    else:
-#        data = None
-#    data = comm.scatter(data, root=0)
-#    print(data)
+if __name__ == "__main__":
     
+    def unit_test_p2p():
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+        
+        A = np.arange(23)
+        B = np.arange(24)[1:24]
+        sigma = 0
+        
+        start_time = 0
+        X = None; Y = None
+        if rank == 0:
+            start_time = time.time()
+            #split the data
+            X = data_splitter(A, size-1) #exclude 0th processor
+            Y = data_splitter(B, size-1)
+            print(X,Y)
+            print("dot result = ",np.dot(A,B))
+            
+        #broadcast the splitted_data
+        X = comm.bcast(X, root=0) 
+        Y = comm.bcast(Y, root=0)
+    #    start_time = comm.bcast(start_time, root=0)
+        
+        ###arbitrary operations start here
+        if rank != 0:
+            y=0
+            for j in range(len(X[rank-1])):
+                y += f_mul(X[rank-1][j], Y[rank-1][j])
+                print("j",j, X[rank-1][j], Y[rank-1][j], y, rank)
+            comm.send(y, dest=0)
+                
+        if rank == 0:
+            for i in range(1, size):
+                y = comm.recv(source=i)
+                print("recv = ",y)
+                sigma += y
+            print("parallel result = ",sigma)
+            print("time = ",time.time()-start_time)
     
+    def unit_test_collective_comm():
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+                
+        y=None
+        r=None
+        length=None
+        if rank==0:
+            length = 100000
+            y = np.linspace(0,1, length)
+            start = time.time()
+        y = comm.bcast(y, root=0)
+        length = comm.bcast(length, root=0)
+        if rank!=0:
+            s_local = time.time()
+            r = 0
+            for i in range(20):
+                r += rank*(dot(y,y))
+            print(rank, r, time.time()-s_local)
+        r = comm.gather(r, root=0)
+        
+        if rank == 0:
+            print("r = ",r)
+            print(time.time()-start)
+        
+    unit_test_collective_comm()
