a19b9c90fa080f8cc2064cc6a90bb2b500919139
berylramadhian <berylramadhian@gmail.com>
2020-07-21 00:08:21 +0700

    added spiral dynamics parallel prototype

--- spiral_dynamics_opt.py
+++ spiral_dynamics_opt.py
@@ -80,11 +80,11 @@ def spiral_dynamics_optimization(F, S, R, m, theta, r, kmax, domain, *f_args, lo
         print()
     
     # generate m init points using random uniform between function domain
-    x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
+#    x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
 
-#    x = sobol_seq.i4_sobol_generate(x_dim, m) #now using sobol
-#    for i in range(x_dim):
-#        x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
+    x = sobol_seq.i4_sobol_generate(x_dim, m) #now using sobol
+    for i in range(x_dim):
+        x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
 
     f = np.array([F(x_, *f_args) for x_ in x])
 
@@ -159,12 +159,12 @@ rotate_point = lambda x, x_star, S, R, x_dim, r, theta: np.matmul( S(R, x_dim, r
 if __name__ == "__main__":
     def unit_tests_single_core():
         start = time.time()
-        f = [lambda x : x[0]**2 + x[1]**2 - 0.3*np.cos(3*np.pi*x[0]) -0.4*np.cos(4*np.pi*x[1]) +0.7]
-        F = lambda x : 1/( 1 + sum([abs(f_(x)) for f_ in f]) )
-        domain = np.array([[-100,100]]*2)    
-        x_star, F_x_star = spiral_dynamics_optimization(F, transformation_matrix, mat_R_ij, 20, 45, 0.95, 500, domain, log=False)
-        print("\nFinal value of x, F(x) is : ",x_star,",",F_x_star,"")
-        print("time = ",time.time()-start)
+#        f = [lambda x : x[0]**2 + x[1]**2 - 0.3*np.cos(3*np.pi*x[0]) -0.4*np.cos(4*np.pi*x[1]) +0.7]
+#        F = lambda x : 1/( 1 + sum([abs(f_(x)) for f_ in f]) )
+#        domain = np.array([[-100,100]]*2)    
+#        x_star, F_x_star = spiral_dynamics_optimization(F, transformation_matrix, mat_R_ij, 20, 45, 0.95, 500, domain, log=False)
+#        print("\nFinal value of x, F(x) is : ",x_star,",",F_x_star,"")
+#        print("time = ",time.time()-start)
         
         f = [lambda x : 2*x[0]+x[1]+x[2]+x[3]+x[4]-6,
              lambda x : 2*x[1]+x[0]+x[2]+x[3]+x[4]-6,
@@ -226,6 +226,8 @@ if __name__ == "__main__":
         
         ####SPO starts here
         x = None
+        x_split = None
+        x_dim = None
         if rank==0:
             start = time.time()
 
@@ -241,32 +243,52 @@ if __name__ == "__main__":
                 print()
             
             # generate m init points using random uniform between function domain
-    #        x = sobol_seq.i4_sobol_generate(x_dim, m) #now using sobol
-    #        for i in range(x_dim):
-    #            x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
-            x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
-            x = data_splitter(x, size-1)
-#            print(x)
-        x = comm.bcast(x, root=0) #broadcast x vectors
-#        print(len(x), rank)
+            x = sobol_seq.i4_sobol_generate(x_dim, m) #now using sobol
+            for i in range(x_dim):
+                x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
+#            x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
+            x_split = data_splitter(x, size) #split data
+        x_split = comm.bcast(x_split, root=0) #broadcast x vectors
+        x_dim = comm.bcast(x_dim, root=0)
         
-        ###############################
-       
-        if rank!=0:
-            fvals = np.array([F(x_) for x_ in x])
-            # search the minimum/maximum (depends on the problem) of f(init_point), in this case, max
-            x_star = x[np.argmax(fvals)]
-                    
-            # rotate the points
-            for k in range(kmax):
-                x = np.array([rotate_point(x[i], x_star, S, R, x_dim, r, theta) for i in range(len(x))])
-                fvals = np.array([F(x_) for x_ in x])
-                x_star_next = x[np.argmax(fvals)]
+        x_pr = x_split[rank-1] #get data based on rank
+        fvals = np.array([F(x_) for x_ in x_pr]) 
+        x_star_pr = x_pr[np.argmax(fvals)]
+        x_star_pr = comm.gather(x_star_pr, root=0)
+        
+        # search the minimum/maximum (depends on the problem) of f(init_point), in this case, max
+        if rank==0:
+            fvals = np.array([F(x_) for x_ in x_star_pr])
+            x_star = x_star_pr[np.argmax(fvals)]
+        else:
+            x_star = None
+        x_star = comm.bcast(x_star, root=0)
+#        # rotate the points
+        for k in range(kmax):
+            x_pr = np.array([rotate_point(x_pr[i], x_star, S, R, x_dim, r, theta) for i in range(len(x_pr))])
+            fvals = np.array([F(x_) for x_ in x_pr])
+            x_star_next_pr = x_pr[np.argmax(fvals)]
+#            print(rank, x_star_next_pr)
+            x_star_next_pr = comm.gather(x_star_next_pr, root=0)
+            ###gather xstars from all ranks
+            if rank==0:
+#                print(x_star_next_pr)
+                fvals = np.array([F(x_) for x_ in x_star_next_pr])
+                x_star_next = x_star_next_pr[np.argmax(fvals)]
                 if(F(x_star_next) > F(x_star)):
                     x_star = np.copy(x_star_next)
-                if log:
-                    print("Iteration\tx_star\tF(x_star)")
-                    print(str(k)+" "+str(x_star)+" "+str(F(x_star)))
+            else:
+                x_star = None
+            x_star = comm.bcast(x_star, root=0)
+#            print(rank, x_star, F(x_star))
+            if log:
+                print("Iteration\tx_star\tF(x_star)")
+                print(str(k)+" "+str(x_star)+" "+str(F(x_star)))        
+#            else:
+#                x_star_next=None
+#            x_star = comm.bcast(x_star, root=0)
+#        
+        if rank==0:
             print(x_star, F(x_star))
             print("time = ",time.time()-start)
 
