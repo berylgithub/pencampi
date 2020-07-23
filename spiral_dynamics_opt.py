d1c10a512d675a4d354a7b3066e383f3beddd52f
berylramadhian <berylramadhian@gmail.com>
2020-07-23 18:20:45 +0700

    wrapped parallelized spiral in a function

--- spiral_dynamics_opt.py
+++ spiral_dynamics_opt.py
@@ -8,9 +8,13 @@ from mpi4py import MPI
 from mpitest import data_splitter
 import numpy as np
 import sobol_seq #if deterministic-ish is needed
-
-
 import time
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
 
 def mat_R_ij(dim, i, j, theta):
     c, s = np.cos(np.deg2rad(theta)), np.sin(np.deg2rad(theta))
@@ -104,55 +108,80 @@ def spiral_dynamics_optimization(F, S, R, m, theta, r, kmax, domain, *f_args, lo
     return x_star, F(x_star, *f_args)
 
 
-def spiral_dynamics_optimization_mpi(F, S, R, m, theta, r, kmax, domain, mpi_params, *f_args, log=True):
+def spiral_dynamics_optimization_mpi(F, S, R, m, theta, r, kmax, domain, *f_args, log=True):
     '''
     Function F optimization using spiral dynamics -> rotating & contracting points
-    mpi_params:
-        {'comm', 'rank', 'size'}
     '''
-    comm = mpi_params['comm']
-    rank = mpi_params['rank']
-    size = mpi_params['size']
 
+    x = None
+    x_split = None
+    x_dim = None
+    if rank==0:
+        start = time.time()
+
+        x_dim = domain.shape[0]
+        
+        if log:
+            print("Init points = ",m)
+            print("Theta = ",theta)
+            print("r = ",r)
+            print("iter_max = ",kmax)
+            print("dimension = ",x_dim)
+            print("domain = ",domain)
+            print()
+        
+        # generate m init points using random uniform between function domain
+        x = sobol_seq.i4_sobol_generate(x_dim, m) #now using sobol
+        for i in range(x_dim):
+            x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
+#            x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
+        x_split = data_splitter(x, size) #split data
+    x_split = comm.bcast(x_split, root=0) #broadcast x vectors
+    x_dim = comm.bcast(x_dim, root=0)
     
-    print(rank)
-
-#    if rank == 0:
-    x_dim = domain.shape[0]
+    x_pr = x_split[rank-1] #get data based on rank
+    fvals = np.array([F(x_) for x_ in x_pr]) 
+    x_star_pr = x_pr[np.argmax(fvals)]
+    x_star_pr = comm.gather(x_star_pr, root=0)
     
-    if log:
-        print("Init points = ",m)
-        print("Theta = ",theta)
-        print("r = ",r)
-        print("iter_max = ",kmax)
-        print("dimension = ",x_dim)
-        print("domain = ",domain)
-        print()
-    
-    # generate m init points using random uniform between function domain
-    x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
-
-#    x = sobol_seq.i4_sobol_generate(x_dim, m) #now using sobol
-#    for i in range(x_dim):
-#        x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
-
-    f = np.array([F(x_, *f_args) for x_ in x])
-
     # search the minimum/maximum (depends on the problem) of f(init_point), in this case, max
-    x_star = x[np.argmax(f)]
-            
-    # rotate the points
+    if rank==0:
+        fvals = np.array([F(x_) for x_ in x_star_pr])
+        x_star = x_star_pr[np.argmax(fvals)]
+    else:
+        x_star = None
+    x_star = comm.bcast(x_star, root=0)
+#        # rotate the points
     for k in range(kmax):
-        x = np.array([rotate_point(x[i], x_star, S, R, x_dim, r, theta) for i in range(len(x))])
-        f = np.array([F(x_, *f_args) for x_ in x])
-        x_star_next = x[np.argmax(f)]
-        if(F(x_star_next) > F(x_star)):
-            x_star = np.copy(x_star_next)
+        x_pr = np.array([rotate_point(x_pr[i], x_star, S, R, x_dim, r, theta) for i in range(len(x_pr))])
+        fvals = np.array([F(x_) for x_ in x_pr])
+        x_star_next_pr = x_pr[np.argmax(fvals)]
+        x_star_next_pr = comm.gather(x_star_next_pr, root=0)
+        ###gather xstars from all ranks
+        if rank==0:
+            fvals = np.array([F(x_) for x_ in x_star_next_pr])
+            x_star_next = x_star_next_pr[np.argmax(fvals)]
+            if(F(x_star_next) > F(x_star)):
+                x_star = np.copy(x_star_next)
+        else:
+            x_star = None
+        x_star = comm.bcast(x_star, root=0)
         if log:
             print("Iteration\tx_star\tF(x_star)")
-            print(str(k)+" "+str(x_star)+" "+str(F(x_star, *f_args)))
-    print(x_star, F(x_star, *f_args))
-    return x_star, F(x_star, *f_args)
+            print(str(k)+" "+str(x_star)+" "+str(F(x_star)))        
+            
+    if rank==0:
+        F_xstar = F(x_star)
+        print(x_star, F_xstar)
+        print("time = ",time.time()-start)
+    else:
+        x_star = None
+        F_xstar = None
+        
+    x_star = comm.bcast(x_star, root=0)
+    F_xstar = comm.bcast(F_xstar, root=0)
+    
+    return x_star, F_xstar
 
 rotate_point = lambda x, x_star, S, R, x_dim, r, theta: np.matmul( S(R, x_dim, r, theta), x ) - np.matmul( ( S(R, x_dim, r, theta) - np.identity(x_dim) ), x_star )
 
@@ -180,10 +209,6 @@ if __name__ == "__main__":
         print("time = ",time.time()-start)
     
     def unit_tests_multi_cores():
-        comm = MPI.COMM_WORLD
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        
         
         f = [lambda x : 2*x[0]+x[1]+x[2]+x[3]+x[4]-6,
              lambda x : 2*x[1]+x[0]+x[2]+x[3]+x[4]-6,
@@ -193,18 +218,17 @@ if __name__ == "__main__":
              ]
         F = lambda x : 1/( 1 + sum([abs(f_(x)) for f_ in f]) )
         domain = np.array([[-10,10]]*5)  
-        start = time.time()
-        mpi_params = {'comm':comm,'rank':rank,'size':size}
-        x_star, F_x_star = spiral_dynamics_optimization_mpi(F, transformation_matrix, mat_R_ij, 10, 45, 0.95, 500, domain, mpi_params, log=False)
-        print("\nFinal value of x, F(x) is : ",x_star,",",F_x_star,"")
-        print("time = ",time.time()-start)
+
+        if rank==0:
+            start = time.time()
+        
+        ### temporary solution just let all cores return identical values copied from core 0
+        xstar, F_xstar = spiral_dynamics_optimization_mpi(F, transformation_matrix, mat_R_ij, 100, 45, 0.95, 500, domain, log=True)
+        if rank == 0:
+            print("\nFinal value of x, F(x) is : ",xstar,",",F_xstar,"")
+            print("time = ",time.time()-start)
     
     def unit_tests_multi_cores_2():
-        comm = MPI.COMM_WORLD
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        
-
         f = [lambda x : 2*x[0]+x[1]+x[2]+x[3]+x[4]-6,
              lambda x : 2*x[1]+x[0]+x[2]+x[3]+x[4]-6,
              lambda x : 2*x[2]+x[0]+x[1]+x[3]+x[4]-6,
@@ -247,6 +271,7 @@ if __name__ == "__main__":
             for i in range(x_dim):
                 x.T[i] = x.T[i]*(domain[i][1]-domain[i][0])+domain[i][0]
 #            x = np.array([[np.random.uniform(domain[j][0], domain[j][1]) for j in range(x_dim)] for i in range(m)])
+            print(x)
             x_split = data_splitter(x, size) #split data
         x_split = comm.bcast(x_split, root=0) #broadcast x vectors
         x_dim = comm.bcast(x_dim, root=0)
@@ -268,11 +293,9 @@ if __name__ == "__main__":
             x_pr = np.array([rotate_point(x_pr[i], x_star, S, R, x_dim, r, theta) for i in range(len(x_pr))])
             fvals = np.array([F(x_) for x_ in x_pr])
             x_star_next_pr = x_pr[np.argmax(fvals)]
-#            print(rank, x_star_next_pr)
             x_star_next_pr = comm.gather(x_star_next_pr, root=0)
             ###gather xstars from all ranks
             if rank==0:
-#                print(x_star_next_pr)
                 fvals = np.array([F(x_) for x_ in x_star_next_pr])
                 x_star_next = x_star_next_pr[np.argmax(fvals)]
                 if(F(x_star_next) > F(x_star)):
@@ -280,21 +303,18 @@ if __name__ == "__main__":
             else:
                 x_star = None
             x_star = comm.bcast(x_star, root=0)
-#            print(rank, x_star, F(x_star))
             if log:
                 print("Iteration\tx_star\tF(x_star)")
                 print(str(k)+" "+str(x_star)+" "+str(F(x_star)))        
-#            else:
-#                x_star_next=None
-#            x_star = comm.bcast(x_star, root=0)
-#        
+                
         if rank==0:
             print(x_star, F(x_star))
             print("time = ",time.time()-start)
 
             
 #    unit_tests_single_core()
-    unit_tests_multi_cores_2()
+    unit_tests_multi_cores()
+#    unit_tests_multi_cores_2()
 
     
     
